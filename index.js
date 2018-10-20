86e317b8aa82b245a6e05ba989eaf5cc4e3bd8e8
Peter Downing <petedowning14@gmail.com>
2018-10-20 16:29:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,10 @@ function destructivelyRemoveFirstKitten() {
   kittens.shift()
 }
 
-function functionName() {
-  
+function appendKitten(name) {
+  return [...kittens, name]
+}
+
+function prependKitten(name) {
+  return [name, ...kittens]
 }
